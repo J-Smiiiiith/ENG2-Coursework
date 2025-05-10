b02d8bb0e9b87c5dfdd1002e3ee5ff59a06585cb
James <jamesas1210@gmail.com>
2025-05-10 17:54:29 +0100

    Create Product entity

---
+++ microservices/product-management/src/main/java/uk/ac/york/eng2/products/domain/Product.java
@@ -0,0 +1,44 @@
+package uk.ac.york.eng2.products.domain;
+
+import io.micronaut.serde.annotation.Serdeable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+@Serdeable
+public class Product {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column float unitPrice;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+}
