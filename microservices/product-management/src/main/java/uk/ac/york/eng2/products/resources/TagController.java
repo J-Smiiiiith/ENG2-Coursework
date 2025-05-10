3f67a46f2b95f9a59e1c328d71ea4195b032037e
James <jamesas1210@gmail.com>
2025-05-10 18:37:16 +0100

    Create Tag controller
    NOTE: Delete does not work because the relationships with other tables have not yet been set

---
+++ microservices/product-management/src/main/java/uk/ac/york/eng2/products/resources/TagController.java
@@ -0,0 +1,63 @@
+package uk.ac.york.eng2.products.resources;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.*;
+import io.micronaut.http.exceptions.HttpStatusException;
+import io.micronaut.transaction.annotation.Transactional;
+import jakarta.inject.Inject;
+import uk.ac.york.eng2.products.domain.Tag;
+import uk.ac.york.eng2.products.dto.TagDTO;
+import uk.ac.york.eng2.products.repository.TagRepository;
+
+import java.net.URI;
+import java.util.List;
+
+@Controller(TagController.PREFIX)
+public class TagController {
+    public static final String PREFIX = "/tags";
+
+    @Inject
+    TagRepository repo;
+
+    @Get
+    public List<Tag> getTags() {
+        return repo.findAll();
+    }
+
+    @Get("/{id}")
+    public Tag getTag(@PathVariable long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Post
+    public HttpResponse<Void> createTag(@Body TagDTO dto) {
+        Tag tag = new Tag();
+        tag.setName(dto.getName());
+        repo.save(tag);
+        return HttpResponse.created(URI.create("/tags/" + tag.getId()));
+    }
+
+    @Put("/{id}")
+    @Transactional
+    public void updateTag(@PathVariable long id, @Body TagDTO dto) {
+        Tag tag = repo.findById(id).orElse(null);
+        if (tag == null) {
+            throw new HttpStatusException(HttpStatus.NOT_FOUND, "Tag not found");
+        } else {
+            tag.setName(dto.getName());
+            repo.save(tag);
+        }
+    }
+
+    @Delete("/{id}")
+    @Transactional
+    public void deleteTag(@PathVariable long id) {
+        Tag tag = repo.findById(id).orElse(null);
+        if (tag == null) {
+            throw new HttpStatusException(HttpStatus.NOT_FOUND, "Tag not found");
+        } else {
+            repo.delete(tag);
+        }
+    }
+}
