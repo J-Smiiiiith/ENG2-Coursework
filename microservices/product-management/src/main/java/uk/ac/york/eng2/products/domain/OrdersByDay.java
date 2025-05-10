812093a7204e35dfcd2603024f6b4f7cf19dc619
James <jamesas1210@gmail.com>
2025-05-10 19:15:32 +0100

    Create OrdersByDay.java

---
+++ microservices/product-management/src/main/java/uk/ac/york/eng2/products/domain/OrdersByDay.java
@@ -0,0 +1,45 @@
+package uk.ac.york.eng2.products.domain;
+
+import io.micronaut.serde.annotation.Serdeable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+@Serdeable
+public class OrdersByDay {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private String day;
+
+    @Column
+    private int count;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
