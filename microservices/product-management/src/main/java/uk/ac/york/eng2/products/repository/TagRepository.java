9626caf198bb00e91ec5fddd4a5c72751d1179e5
James <jamesas1210@gmail.com>
2025-05-10 18:26:29 +0100

    Create Tag repository

---
+++ microservices/product-management/src/main/java/uk/ac/york/eng2/products/repository/TagRepository.java
@@ -0,0 +1,8 @@
+package uk.ac.york.eng2.products.repository;
+
+import io.micronaut.data.repository.PageableRepository;
+import uk.ac.york.eng2.products.domain.Tag;
+
+public interface TagRepository extends PageableRepository<Tag, Long> {
+
+}
