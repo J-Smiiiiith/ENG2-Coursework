2731d5f20cb878edf0b5dd345e0e1ee89e115ce7
James <jamesas1210@gmail.com>
2025-05-13 15:21:05 +0100

    Create OrderItemController.java

---
+++ microservices/order-management/src/main/java/uk/ac/york/eng2/orders/resources/OrderItemController.java
@@ -0,0 +1,44 @@
+package uk.ac.york.eng2.orders.resources;
+
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import jakarta.inject.Inject;
+import uk.ac.york.eng2.orders.domain.OrderItem;
+import uk.ac.york.eng2.orders.dto.OrderItemDTO;
+import uk.ac.york.eng2.orders.repository.OrderItemRepository;
+
+import java.net.URI;
+import java.util.List;
+
+@Controller(OrderItemController.PREFIX)
+public class OrderItemController {
+    public static final String PREFIX = "/order-items";
+
+    @Inject
+    OrderItemRepository orderItemRepo;
+
+    @Get
+    public List<OrderItem> getOrderItems() {
+        return orderItemRepo.findAll();
+    }
+
+    @Get("/{id}")
+    public OrderItem getOrderItem(Long id) {
+        return orderItemRepo.findById(id).orElse(null);
+    }
+
+    @Post
+    public HttpResponse<Void> createOrderItem(@Body OrderItemDTO dto) {
+        // get product from client and check its validity
+        OrderItem orderItem = new OrderItem();
+        orderItem.setProductId(dto.getProductId());
+        orderItem.setQuantity(dto.getQuantity());
+        orderItem.setOrderId(dto.getOrderId());
+        orderItem.setUnitPrice(0); //temp - will be taken from product client
+        orderItemRepo.save(orderItem);
+        return HttpResponse.created(URI.create(PREFIX + "/" + orderItem.getId()));
+    }
+}
