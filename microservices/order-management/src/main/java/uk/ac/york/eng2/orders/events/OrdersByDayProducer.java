79fc64be9c67a6c8ec5bb3bba7d7634e3e072cee
James <jamesas1210@gmail.com>
2025-05-14 15:20:43 +0100

    Create OrdersByDayProducerTest.java

--- microservices/order-management/src/main/java/uk/ac/york/eng2/orders/events/OrdersByDayProducer.java
+++ microservices/order-management/src/main/java/uk/ac/york/eng2/orders/events/OrdersByDayProducer.java
@@ -1,8 +1,10 @@
 package uk.ac.york.eng2.orders.events;
 
+import io.micronaut.configuration.kafka.annotation.KafkaClient;
 import io.micronaut.configuration.kafka.annotation.KafkaKey;
 import io.micronaut.configuration.kafka.annotation.Topic;
 
+@KafkaClient
 public interface OrdersByDayProducer {
 
     @Topic(OMTopicFactory.ORDERS_BY_DAY_TOPIC)
