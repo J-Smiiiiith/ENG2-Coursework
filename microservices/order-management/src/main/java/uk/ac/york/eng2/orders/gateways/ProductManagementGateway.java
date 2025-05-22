ac9c36bb60c844cb3c6c79d5524549d3ea4597a6
James <jamesas1210@gmail.com>
2025-05-22 16:54:40 +0100

    Delete old non-working files

--- microservices/order-management/src/main/java/uk/ac/york/eng2/orders/gateways/ProductManagementGateway.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.york.eng2.orders.gateways;
-
-import java.util.Map;
-
-public interface ProductManagementGateway {
-    Map<String, Map<Long, Integer>> checkProductsValidity(Map<Long, Integer> products);
-    float getProductsPrice(Map<Long, Integer> products);
-    float getProductUnitPrice(long productId);
-}
--- microservices/product-management/src/main/resources/db/migration/V1__create-products-schema.sql
+++
@@ -1,7 +0,0 @@
-create sequence hibernate_sequence;
-
-create table product (
-    id bigint not null primary key,
-    name varchar(255) not null,
-    unit_price decimal(12, 2) not null check (unit_price >= 0)
-);
