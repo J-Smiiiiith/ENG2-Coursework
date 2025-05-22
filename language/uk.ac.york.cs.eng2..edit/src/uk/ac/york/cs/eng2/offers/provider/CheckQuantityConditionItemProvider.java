bc573203cdad82ad09e0039b7aca185b0099c1d6
James <jamesas1210@gmail.com>
2025-05-22 17:00:26 +0100

    Fix generator (#27)
    * Delete old non-working files
    
    * Add in new working files

---
+++ language/uk.ac.york.cs.eng2..edit/src/uk/ac/york/cs/eng2/offers/provider/CheckQuantityConditionItemProvider.java
@@ -0,0 +1,132 @@
+/**
+ */
+package uk.ac.york.cs.eng2.offers.provider;
+
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+import uk.ac.york.cs.eng2.offers.CheckQuantityCondition;
+import uk.ac.york.cs.eng2.offers.OffersPackage;
+
+/**
+ * This is the item provider adapter for a {@link uk.ac.york.cs.eng2.offers.CheckQuantityCondition} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class CheckQuantityConditionItemProvider extends ConditionItemProvider {
+	/**
+	 * This constructs an instance from a factory and a notifier.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public CheckQuantityConditionItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addQuantityPropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Quantity feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addQuantityPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_CheckQuantityCondition_quantity_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_CheckQuantityCondition_quantity_feature", "_UI_CheckQuantityCondition_type"),
+				 OffersPackage.Literals.CHECK_QUANTITY_CONDITION__QUANTITY,
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.INTEGRAL_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+	 * This returns CheckQuantityCondition.gif.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/CheckQuantityCondition"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String getText(Object object) {
+		String label = ((CheckQuantityCondition)object).getName();
+		return label == null || label.length() == 0 ?
+			getString("_UI_CheckQuantityCondition_type") :
+			getString("_UI_CheckQuantityCondition_type") + " " + label;
+	}
+
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached
+	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(CheckQuantityCondition.class)) {
+			case OffersPackage.CHECK_QUANTITY_CONDITION__QUANTITY:
+				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+				return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+	 * that can be created under this object.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+	}
+
+}
--- microservices/product-management/src/main/java/uk/ac/york/eng2/products/events/ProductDayQuantity.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.york.eng2.products.events;
-
-import io.micronaut.serde.annotation.Serdeable;
-
-@Serdeable
-public record ProductDayQuantity(long productId, String day, int quantity) {
-}
